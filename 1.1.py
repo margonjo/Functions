4f509f65fbb1f30c50cbc406ec2aec3a4df0f81a
40756 <40756@PC004143.coll.lr.local>
2014-11-27 12:16:48 +0000

    1.1

---
+++ 1.1.py
@@ -0,0 +1,12 @@
+#Marc Gonzalez
+#27/11/14
+#1.1
+
+
+
+def Output_symbols():
+    N = int(input("please enter a number: "))
+    C = input("please enter a character: ")
+    total = N * C
+    print(total)
+Output_symbols()
